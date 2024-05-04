694669d5d31cee79e08d5f65c74f0061bdf1a069
fsansotta <sansottafariel@gmail.com>
2024-05-04 10:41:11 -0300

    realizacion de navbar, cartwidget e itemlistcontainer

---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const ItemListContainer = ({ props }) => {
+  return (
+    <div>
+      <p>BIENVENIDO {props}</p>
+    </div>
+
+
+  )
+
+
+
+
+}
+
+export default ItemListContainer
