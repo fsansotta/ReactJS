f05b222624c9e7f12026fbba20f253e8041c1fba
fsansotta <140967694+fsansotta@users.noreply.github.com>
2024-06-25 19:01:28 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -21,9 +21,9 @@ function App() {
         <BrowserRouter>
           <NavBar />         
           <Routes>
-            <Route path="/" element={<ItemListContainer props={'a la tienda del Rey de Copas'} />} />
+            <Route path="/" element={<ItemListContainer props={'Bienvenido a la tienda del Rey de Copas'} />} />
             <Route path="/producto/:productoId" element={<ItemDetailContainer />} />
-            <Route path="/categoria/:categoria" element={<ItemListContainer props={'a la tienda del Rey de Copas'} />} />     
+            <Route path="/categoria/:categoria" element={<ItemListContainer props={'Bienvenido a la tienda del Rey de Copas'} />} />        
             <Route path="/cart" element={<Cart />} />
             <Route path="/checkout" element={<CheckOutContainer/>} />
           </Routes>
