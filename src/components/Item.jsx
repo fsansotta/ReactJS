47396a00ddb7bfca88f69bb544548e1b75c85991
fsansotta <140967694+fsansotta@users.noreply.github.com>
2024-06-25 19:02:32 -0300

    Update Item.jsx

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -4,11 +4,11 @@ import { Link } from "react-router-dom"
 const Item = ({ producto }) => {
     return (
         <div className='cardPadre'>
-            <div className="card" style={{ width: "18rem" }}>
+            <div className="card" style={{ alignItems:"center", width: "18rem" }}>
                 <img src={producto.imagen} class="card-img-top" alt="camiseta"></img>
                 <div class="card-body">
                     <h5 class="card-title">{producto.nombre}</h5>
-                    <p class="card-text">{producto.precio}</p>
+                    <p class="card-text">${producto.precio}</p>
                     <Link to={`/producto/${producto.id}`}> <button class="btn btn-danger" style={{ background: "#e10404" }}>Ver producto</button> </Link>
                 </div>
             </div>
